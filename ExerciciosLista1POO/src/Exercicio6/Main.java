524b742ad59295e34fc0069dac42b68382fb8784
Matheus Arendartchuk <150072083+Theuzoz@users.noreply.github.com>
2024-03-23 18:09:33 -0300

    Update Main.java

--- ExerciciosLista1POO/src/Exercicio6/Main.java
+++ ExerciciosLista1POO/src/Exercicio6/Main.java
@@ -9,8 +9,11 @@ package Exercicio6;
  * que deve possuir um argumento inteiro, no caso aqui, 100.*/
 
 public class Main {
-
+	
 	public static void main(String[] args) {
+		AdivinhaNum jogo = new AdivinhaNum();
+		
+		jogo.sortear();
+		jogo.adivinhar();
 	}
-
 }
