b0933a97f44ec7419d49c14a9a1b721eb236577b
Matheus Arendartchuk <150072083+Theuzoz@users.noreply.github.com>
2024-04-07 22:22:13 -0300

    Update Produto.java

--- SimuladoJavaPOO/src/ex03/Produto.java
+++ SimuladoJavaPOO/src/ex03/Produto.java
@@ -5,10 +5,10 @@ public class Produto {
 	private double valor;
 	
 	public Produto(String objeto, double valor, String tipo){
-        this.objeto = objeto;
-        this.valor = valor;
-        this.tipo = tipo;
-    }
+        	this.objeto = objeto;
+        	this.valor = valor;
+        	this.tipo = tipo;
+    	}
 	
 	public void mostrarProduto() {
 		System.out.println("produtoNome: "+objeto+", preço: "+valor+", tipo: "+tipo);
