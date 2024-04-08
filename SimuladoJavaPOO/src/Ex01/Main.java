ca494b97bcceab5d98d00645e9f2adc8c1996f1e
Matheus Arendartchuk <150072083+Theuzoz@users.noreply.github.com>
2024-04-07 21:03:42 -0300

    Update Main.java

--- SimuladoJavaPOO/src/Ex01/Main.java
+++ SimuladoJavaPOO/src/Ex01/Main.java
@@ -1,5 +1,12 @@
 package Ex01;
 
+/* Exercicio 1.
+ * Um passageiro possui nome e pode ser estudante ou não.
+ * Em um onibus, passageiros podem ser alocados no sistema de compra de passagem
+ * ao onibus. O preço da passagem é definido para cada onibus e sabe-se que um
+ * passageiro estudante paga meia. O sistema pode listar os passageiros, listar os
+ * estudantes apenas e calcular o total de venda de passagens. */
+
 public class Main {
 	public static void main(String[] args) {
 		Passageiro p1 = new Passageiro("Matheus", true);
